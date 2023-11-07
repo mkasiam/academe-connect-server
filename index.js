325fcb25e2d4ebd818c6922df6fcc2c9a68e9e1b
mkasiam <kawsarahmmed10001@gmail.com>
2023-11-07 11:47:25 +0600

    all update related to submit is done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 //using middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://academe-connect.web.app"],
+    origin: ["http://localhost:5173", "https://academe-connect.web.app"],
     credentials: true,
   })
 );
@@ -52,19 +52,19 @@ async function run() {
       const result = await assignmentCollection.findOne(query);
       res.send(result);
     });
-    app.put("/assignments/:id", async (req, res)=>{
+    app.put("/assignments/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updatedInfo = req.body;
-      const options = {upsert:true};
+      const options = { upsert: true };
       const assignment = {
         $set: {
           title: updatedInfo.title,
           marks: updatedInfo.marks,
           thumbnail: updatedInfo.thumbnail,
           dueDate: updatedInfo.dueDate,
-          details:updatedInfo.details,
-          difficulty:updatedInfo.difficulty
+          details: updatedInfo.details,
+          difficulty: updatedInfo.difficulty,
         },
       };
       const result = await assignmentCollection.updateOne(
@@ -73,7 +73,7 @@ async function run() {
         options
       );
       res.send(result);
-    })
+    });
     app.delete("/assignments/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -81,36 +81,50 @@ async function run() {
       res.send(result);
     });
     // Submitted Assignments
-    app.post("/submittedAssignments",async(req,res)=>{
+    app.post("/submittedAssignments", async (req, res) => {
       const submittedAssignment = req.body;
-      const result = await submittedAssignmentCollection.insertOne(submittedAssignment);
+      const result = await submittedAssignmentCollection.insertOne(
+        submittedAssignment
+      );
       res.send(result);
-    })
-    app.get("/submittedAssignments",async(req,res)=>{
-      const result = await submittedAssignmentCollection.find().toArray();
-      res.send(result);
-    })
-    app.get("/submittedAssignments",async(req,res)=>{
-      const query = { status: req.status };
+    });
+    app.get("/submittedAssignments", async (req, res) => {
+      const status = req.query.status;
+      const email = req.query.email; // Get the email from the query parameter
+    
+      const query = {}; // Initialize an empty query object
+    
+      if (status === "pending") {
+        query.status = "pending";
+      }
+    
+      if (email) {
+        query.submittedUserEmail = email; // Add the email condition to the query
+      }
+    
+      // Query assignments based on the combined conditions
       const result = await submittedAssignmentCollection.find(query).toArray();
       res.send(result);
-    })
-    app.put("/submittedAssignments/:id",async(req,res)=>{
+    });
+    app.put("/submittedAssignments/:id", async (req, res) => {
       const submittedAssignment = req.body;
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
-      const options = {upsert:true};
+      const options = { upsert: true };
       const updateDoc = {
         $set: {
           status: submittedAssignment.status,
-          obtainMarks:submittedAssignment.obtainMarks,
-          feedback:submittedAssignment.feedback
+          obtainMarks: submittedAssignment.obtainMarks,
+          feedback: submittedAssignment.feedback,
         },
       };
-      const result = await submittedAssignmentCollection.updateOne(filter, updateDoc,options);
+      const result = await submittedAssignmentCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
-      
-    })
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
